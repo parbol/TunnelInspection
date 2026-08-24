9225242914f4cf141e6676677f344f56ebfa3c5a
parbol <pablom@cern.ch>
2026-08-24 23:36:01 +0200

    Algoritmo clasico

--- Analysis/src/ActiveVolume.py
+++ Analysis/src/ActiveVolume.py
@@ -12,10 +12,19 @@ class ActiveVolume:
         self.center = np.asarray(center)
         self.size = np.asarray(size)
         self.NVoxels = np.asarray(NVoxels, dtype=np.uint32)
-        self.Linit = self.center - self.size/2.0
+        self.Lmin = self.center - self.size/2.0
+        self.Lmax = self.center + self.size/2.0
         self.step = self.size/self.NVoxels
         self.voxels = []
         ################
+        x = [] 
+        for ix in range(NVoxels[0]):
+            x.append(self.Linit[0] + ix * self.step[0])
+        self.x = np.asarray(x)
+        y = [] 
+        for iy in range(NVoxels[1]):
+            y.append(self.Linit[1] + iy * self.step[1])
+        self.y = np.asarray(y)
         z = [] 
         for iz in range(NVoxels[2]):
             z.append(self.Linit[2] + iz * self.step[2])
@@ -34,20 +43,28 @@ class ActiveVolume:
             self.voxels.append(voxelsYZ)    
 
 
-    def voxelList(self, rays):
-        z = np.tile(self.z, (rays.shape[0], 1))
-        z = z.T
-        z = np.reshape(z, (z.shape[0]*z.shape[1], 1))
-        raysl = np.tile(rays, (self.z.shape[0], 1))  
-        x0 = np.asmatrix(raysl[:,0]).T
-        y0 = np.asmatrix(raysl[:,1]).T
-        z0 = np.asmatrix(raysl[:,2]).T
-        vx = np.asmatrix(raysl[:,3]).T
-        vy = np.asmatrix(raysl[:,4]).T
-        vz = np.asmatrix(raysl[:,5]).T
-        l = (z - z0)/vz
+    def voxelList(self, ray):
+
+        epsilon = 1e-6
+        x0 = ray[0]
+        y0 = ray[1]
+        z0 = ray[2]
+        vx = ray[3]
+        vy = ray[4]
+        vz = ray[5]
+        lx = (self.x - x0)/vx 
+        ly = (self.y - y0)/vy  
+        lz = (self.z - z0)/vz 
+        l = np.concatenate((lx,ly,lz))
+        l = np.sort(l)
         x = x0 + np.multiply(l,vx)
         y = y0 + np.multiply(l,vy)
+        z = z0 + np.multiply(l,vz)
+        r = np.concatenate((x, y, z), axis=1)
+        r = r[(r[0] > self.Lmin[0]) & (r[0] < self.Lmax[0]) & 
+              (r[1] > self.Lmin[1]) & (r[1] < self.Lmax[1]) & 
+              (r[2] > self.Lmin[2]) & (r[2] < self.Lmax[2])]
+        nx = 
         z = np.reshape(z, (self.z.shape[0], rays.shape[0])).T
         x = np.reshape(x, (self.z.shape[0], rays.shape[0])).T
         y = np.reshape(y, (self.z.shape[0], rays.shape[0])).T
